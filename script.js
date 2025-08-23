09887a27039535f7db3a987b73047f59760d9cb3
Daniil Melnik <melnikdaniil03gmail.com>
2025-08-23 11:01:54 +0300

    JS training 1

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ const goodbye = 'Пока'
 const message = `${greeting}, ${name}!`
 
 console.log(message)
+
+const age = +prompt('Сколько тебе лет?')
+
+switch (true) {
+    case age < 18: {
+        console.log('Доступ закрыт!')
+        break;1
+    }
+
+    case age <= 125: {
+        console.log('Доступ разрешен!')
+        break;
+    }
+
+    case age >= 126: {
+        console.log('Вампир что ли?')
+        break;
+    }
+
+    default: {
+        console.log('Возраст введен некоректно!')
+        break;
+    }
+}
